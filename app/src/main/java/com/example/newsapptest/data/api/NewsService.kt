040908f3f3da59a0fb18e056c80fbcc653abca08
Ivan <ivantihu@gmail.com>
2023-03-12 21:46:10 +0200

    Commit 3.1 Add Retrofit and DI

--- app/src/main/java/com/example/newsapptest/data/api/NewsService.kt
+++ app/src/main/java/com/example/newsapptest/data/api/NewsService.kt
@@ -1,4 +1,22 @@
 package com.example.newsapptest.data.api
 
+import com.example.newsapptest.utils.Constants.Companion.API_KEY
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface NewsService {
+
+    @GET("/v2/everything")
+    suspend fun getEverything(
+        @Query("q") query: String,
+        @Query("page") page: Int = 1,
+        @Query("apiKey") apiKey: String = API_KEY
+    )
+
+    @GET
+    suspend fun getHeadLines(
+        @Query("country") countryCode: String = "us",
+        @Query("page") page: Int = 1,
+        @Query("apiKey") apiKey: String = API_KEY
+    )
 }
