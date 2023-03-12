d5fc6b30a589a9a9f93941eadd4e5073cd2e05aa
Ivan <ivantihu@gmail.com>
2023-03-12 20:49:12 +0200

    Commit 2 Bottom Navigation

--- app/src/main/java/com/example/newsapptest/MainActivity.kt
+++ app/src/main/java/com/example/newsapptest/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.newsapptest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.example.newsapptest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private  var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.fragment_splash)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        Handler(Looper.myLooper()!!).postDelayed({
+            setContentView(binding.root)
+            setupWithNavController(binding.bottomNavMenu, navController)
+        }, 5000)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
---
+++ app/src/main/java/com/example/newsapptest/ui/main/MainFragment.kt
@@ -0,0 +1,25 @@
+package com.example.newsapptest.ui.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.newsapptest.R
+import com.example.newsapptest.databinding.FragmentDetailsBinding
+import com.example.newsapptest.databinding.FragmentMainBinding
+
+
+class MainFragment : Fragment() {
+
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMainBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+}
